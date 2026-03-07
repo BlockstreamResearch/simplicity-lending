12c6c4161e015400860e91477697d888544cbcea
Kyryl R <kyryl.ryabov@gmail.com>
2026-03-07 07:51:01 +0200

    contracts: pre lock: move witness sig out of branch

--- crates/contracts/src/pre_lock/build_witness.rs
+++ crates/contracts/src/pre_lock/build_witness.rs
@@ -11,7 +11,7 @@ use simplicityhl::{
 pub enum PreLockBranch<'a> {
     // Left(())
     LendingCreation,
-    // Right(Signature)
+    // Right(())
     PreLockCancellation {
         cancellation_signature: &'a bitcoin::secp256k1::schnorr::Signature,
     },
@@ -24,24 +24,34 @@ pub enum PreLockBranch<'a> {
 #[must_use]
 pub fn build_pre_lock_witness(branch: PreLockBranch) -> WitnessValues {
     let lending_creation = ResolvedType::parse_from_str("()").unwrap();
-    let pre_lock_cancellation = ResolvedType::parse_from_str("Signature").unwrap();
+    let pre_lock_cancellation = ResolvedType::parse_from_str("()").unwrap();
 
     let path_type = ResolvedType::either(lending_creation, pre_lock_cancellation);
 
-    let branch_str = match branch {
+    let path = match branch {
         PreLockBranch::LendingCreation => "Left(())".to_string(),
-        PreLockBranch::PreLockCancellation {
-            cancellation_signature,
-        } => {
-            format!(
-                "Right({})",
-                Value::byte_array(cancellation_signature.serialize()),
-            )
-        }
+        PreLockBranch::PreLockCancellation { .. } => "Right(())".to_string(),
     };
 
-    simplicityhl::WitnessValues::from(HashMap::from([(
+    let mut values = HashMap::from([(
         WitnessName::from_str_unchecked("PATH"),
-        simplicityhl::Value::parse_from_str(&branch_str, &path_type).unwrap(),
-    )]))
+        simplicityhl::Value::parse_from_str(&path, &path_type).unwrap(),
+    )]);
+
+    if let PreLockBranch::PreLockCancellation {
+        cancellation_signature,
+    } = branch
+    {
+        values.insert(
+            WitnessName::from_str_unchecked("CANCELLATION_SIGNATURE"),
+            Value::byte_array(cancellation_signature.serialize()),
+        );
+    } else {
+        values.insert(
+            WitnessName::from_str_unchecked("CANCELLATION_SIGNATURE"),
+            Value::byte_array([0; 64]),
+        );
+    }
+
+    simplicityhl::WitnessValues::from(values)
 }
