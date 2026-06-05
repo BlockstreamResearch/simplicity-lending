20eb122f6155cee16d7fc60d1d942b19175a5abf
lilbonekit <mykytasydorenko0@gmail.com>
2026-06-05 17:20:45 +0300

    feat: add AppErrorBoundary for error handling in app

--- web-v2/src/main.tsx
+++ web-v2/src/main.tsx
@@ -5,9 +5,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
+import { AppErrorBoundary } from './components/AppErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppErrorBoundary>
+      <App />
+    </AppErrorBoundary>
   </StrictMode>,
 )
