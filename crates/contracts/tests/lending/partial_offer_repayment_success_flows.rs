df1c605e39f106e928987b48faeff0b685c7c38e
Oleh Komendant <44612825+Hrom131@users.noreply.github.com>
2026-08-14 19:16:58 +0300

    Fix/protocol fee vault params (#150)

--- crates/contracts/tests/lending/partial_offer_repayment_success_flows.rs
+++ crates/contracts/tests/lending/partial_offer_repayment_success_flows.rs
@@ -459,3 +459,107 @@ fn partial_repayment_succeeds_in_principal_repayment_phase(
 
     Ok(())
 }
+
+#[simplex::test]
+fn partial_repayment_succeeds_after_several_repayments(
+    context: simplex::TestContext,
+) -> anyhow::Result<()> {
+    let provider = context.get_default_provider();
+    let borrower = context.get_default_signer();
+    let lender = context.random_signer();
+
+    let (mut offer, offer_parameters) = default_partial_repayment_setup(&context, &lender)?;
+
+    let total_fee_to_repay = offer_parameters.offer_parameters.get_total_fee();
+    let amount_to_repay = total_fee_to_repay / 2;
+
+    partial_repay_offer(&context, &mut offer, borrower, amount_to_repay)?;
+
+    assert_eq!(
+        offer_parameters
+            .offer_parameters
+            .get_repayment_phase(offer.get_current_debt()),
+        OfferRepaymentPhase::RepayingOfferFee
+    );
+
+    let amount_to_repay = total_fee_to_repay * 2;
+
+    let collateral_to_unlock = amount_to_repay
+        * offer_parameters.offer_parameters.collateral_amount
+        / offer_parameters
+            .offer_parameters
+            .get_total_amount_to_repay();
+
+    let active_offer_utxo = provider.fetch_scripthash_utxos(&offer.get_script_pubkey())?[0].clone();
+    let borrower_nft_utxo =
+        borrower.get_utxos_asset(offer_parameters.borrower_nft_asset_id)?[0].clone();
+    let (lender_vault_utxo, protocol_fee_vault_utxo) =
+        get_active_offer_vaults_utxos(&context, &offer)?;
+
+    let borrower_principal_utxo =
+        borrower.get_utxos_asset(offer_parameters.principal_asset_id)?[0].clone();
+
+    let principal_utxo_amount = borrower_principal_utxo.explicit_amount();
+
+    assert!(principal_utxo_amount >= amount_to_repay);
+    assert!(lender_vault_utxo.is_some());
+    assert!(protocol_fee_vault_utxo.is_some());
+
+    let mut ft = FinalTransaction::new();
+
+    ft.add_input(
+        PartialInput::new(borrower_nft_utxo),
+        RequiredSignature::NativeEcdsa,
+    );
+    ft.add_output(PartialOutput::new(
+        borrower.get_address().script_pubkey(),
+        1,
+        offer_parameters.borrower_nft_asset_id,
+    ));
+
+    offer.attach_partial_repayment(
+        &mut ft,
+        active_offer_utxo,
+        lender_vault_utxo,
+        protocol_fee_vault_utxo,
+        amount_to_repay,
+    );
+
+    ft.add_input(
+        PartialInput::new(borrower_principal_utxo),
+        RequiredSignature::NativeEcdsa,
+    );
+
+    ft.add_output(
+        PartialOutput::new(
+            borrower.get_address().script_pubkey(),
+            collateral_to_unlock,
+            offer_parameters.collateral_asset_id,
+        )
+        .with_blinding_key(borrower.get_blinding_public_key()),
+    );
+
+    if principal_utxo_amount > amount_to_repay {
+        ft.add_output(PartialOutput::new(
+            borrower.get_address().script_pubkey(),
+            principal_utxo_amount - amount_to_repay,
+            offer_parameters.principal_asset_id,
+        ));
+    }
+
+    borrower.broadcast(&ft)?.wait()?;
+
+    let already_repaid_amount = offer_parameters
+        .offer_parameters
+        .get_already_repaid_amount(offer.get_current_debt());
+    let already_repaid_protocol_fee = offer_parameters.offer_parameters.get_total_protocol_fee();
+
+    check_vault_balances(
+        &context,
+        &offer,
+        already_repaid_amount - already_repaid_protocol_fee,
+        already_repaid_protocol_fee,
+    )?;
+
+    Ok(())
+}
